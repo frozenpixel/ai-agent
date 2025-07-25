d2a4ca965372497ba8dcfca092c5c9c462467dd7
frozenpixel <ashley.malone@gmail.com>
2025-07-25 16:55:39 -0700

    add get_file_content function

---
+++ functions/get_file_content.py
@@ -0,0 +1,33 @@
+import os
+from config import MAX_CHARS
+
+def get_file_content(working_directory, file_path):
+    path = os.path.join(working_directory, file_path)
+    absolute_path = os.path.abspath(path)
+    print(absolute_path)
+
+    if absolute_path.startswith(os.path.abspath(working_directory)) == False:
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    
+    if os.path.isfile(path) == False:
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+    try:
+        with open(path, "r") as f:
+            file_content = f.read()
+            check_content = file_content.strip().split()
+            char_len = sum(len(word) for word in check_content)
+
+            if char_len > MAX_CHARS:
+                return file_content + f'[...File "{file_path}" truncated at 10000 characters]'
+            
+    except Exception as e:
+        return f'Error: {e}'
+    
+    try:
+        with open(path, "r") as f:
+            file_content = f.read(MAX_CHARS)
+
+            return file_content
+    except Exception as e:
+        return f'Error: {e}'
--- tests.py
+++ tests.py
@@ -1,12 +1,24 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
-print(get_files_info("calculator", "."))
 
-print(get_files_info("calculator", "pkg"))
+# print(get_files_info("calculator", "."))
 
-print(get_files_info("calculator", "/bin"))
+# print(get_files_info("calculator", "pkg"))
 
-print(get_files_info("calculator", "../"))
+# print(get_files_info("calculator", "/bin"))
 
-if __name__ == "__main__":
-    test()
+# print(get_files_info("calculator", "../"))
+
+print(get_file_content("calculator", "lorem.txt"))
+
+print(get_file_content("calculator", "main.py"))
+
+print(get_file_content("calculator", "pkg/calculator.py"))
+
+print(get_file_content("calculator", "/bin/cat"))
+
+print(get_file_content("calculator", "pkg/does_not_exist.py"))
+
+# if __name__ == "__main__":
+#     test()
